c35c2bcac04db1d030c43a6c830ba9ade8422a52
tmoore <tmoore@fd756710-30ea-0310-9ad9-c667adb881b7>
2008-01-23 02:39:52 +0000

    added sequence

---
+++ NCDFSequence.m
@@ -0,0 +1,14 @@
+//
+//  NCDFSequence.m
+//  netcdf
+//
+//  Created by Thomas Moore on 5/18/07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import "NCDFSequence.h"
+
+
+@implementation NCDFSequence
+
+@end
